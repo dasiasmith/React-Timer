3cbe91b92b0740a79d5d1bc0b93a3084af3702ec
Dasia <dasiasmith023@gmail.com>
2022-06-08 23:11:48 -0500

    Initialized app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CountdownTimer from './components/countdownTimer/countdownTimer';
+
+function App() {
+  return (
+    <div className="App">
+      <CountdownTimer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/countdownTimer/countdownTimer.js
@@ -0,0 +1,7 @@
+const CountdownTimer = () => {
+    return(
+        <span>Hello World</span>
+    );
+}
+
+export default CountdownTimer;
